ba81adec20046fefeb011b5ab6955cc61c6ba432
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-11-14 09:25:20 +0600

    sign in and out added with conditional rendaring

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,40 @@
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [user, setUser] = useState({});
+
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const handleClick =() => {
+    const handleClick = () => {
         signInWithPopup(auth, provider)
-        .then(result => {
-            console.log(result.user)})
-        .catch(error => {
-            console.log(error.message)})
+            .then(result => {
+                setUser(result.user)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
     }
 
+    const handleSignout = () => {
+        signOut(auth)
+            .then()
+            .catch(error => console.log(error.message))
+            setUser(null);
+    }
 
     return (
         <div>
-            <button onClick={handleClick} className='btn btn-success'>Google</button>
+            {!user ? <button onClick={handleClick} className='btn btn-success'>Google</button>
+            : <div onClick={handleSignout} className="btn btn-warning">Signout</div>}
+           { user && <div className="mx-auto">
+                <h3>User: {user.displayName}</h3>
+                <h4>User Email: {user.email}</h4>
+                <img src={user.photoURL} alt="" />
+            </div>}
         </div>
     );
 };
